9111096d6d511497f4feb0a9c56636794ad90691
moses <fanshaorong@163.com>
2019-05-04 11:21:39 +0800

    update

--- src/main/java/com/ibm/Test.java
+++ src/main/java/com/ibm/Test.java
@@ -1,13 +1,12 @@
 /*
  * Copyright © 2019 bjfansr@cn.ibm.com Inc. All rights reserved
- * @Description: com.ibm.Test
+ * @description: com.ibm.Test
  * @version V1.0
  */
 package com.ibm;
 
 /**
  * @author Moses
- * @Date 2019/5/3
  */
 public class Test {
     public void test(){
